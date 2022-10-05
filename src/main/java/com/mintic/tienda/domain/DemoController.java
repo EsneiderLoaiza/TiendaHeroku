4bd0c996dd0311c9c81b93ad75448f26134c9fdc
esneider-ies- <david.loaiza@iesonline.com.co>
2022-10-05 10:13:42 -0500

    feat: Se agrega CorssOrigin="*" para el controller

--- src/main/java/com/mintic/tienda/domain/DemoController.java
+++ src/main/java/com/mintic/tienda/domain/DemoController.java
@@ -3,6 +3,7 @@ package com.mintic.tienda.domain;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import com.mintic.tienda.service.DTO.PrendaDTO;
 import com.mintic.tienda.service.moduleclothes.IManageClothesService;
 import com.mintic.tienda.service.moduleclothes.exceptions.ManagerClothesServiceException;
 
+@CrossOrigin(origins = "*")
 @Controller
 public class DemoController {
     
